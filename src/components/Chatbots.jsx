7fe4d6f997c05b1a376e72deea9ad63b7aedfb38
mahsaSH717 <mahsa.sh717@gmail.com>
2024-03-11 12:32:38 +0100

    fix typo

--- src/components/Chatbots.jsx
+++ src/components/Chatbots.jsx
@@ -7,7 +7,7 @@ function Chatbots(){
     return(
         <div className={styles.links}>
          <Button className={styles.linkBtn} href="https://chat.openai.com/" target="_blank" variant="link" >ChatGPT</Button>
-         <Button className={styles.linkBtn} href="https://gemini.google.com/app" target="_blank" variant="link" >Gimini</Button>
+         <Button className={styles.linkBtn} href="https://gemini.google.com/app" target="_blank" variant="link" >Gemini</Button>
         </div>
     );
 }
